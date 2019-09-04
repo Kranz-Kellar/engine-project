a280933e88b96c8c1f46f2d37dd8c29343e308f6
Andrew <kitan012@mail.ru>
2019-09-04 08:19:25 +0300

    Add project files.

---
+++ ECS/Entity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineProject.ECS
+{
+    public class Entity
+    {
+        private int id;
+        private string name;
+
+        private List<Component> components;
+
+        public Entity(string name)
+        {
+            this.name = name;
+            this.components = new List<Component>();
+
+            //Generate unique id
+            this.id = 0;
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public Component addComponent(Component component)
+        {
+            components.Add(component);
+
+            return component;
+        }
+
+        public Component getComponent()
+        {
+
+        }
+    }
+}
---
+++ ECS/IEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineProject.ECS
+{
+    public interface IEntity
+    {
+        void Init();
+        void IsEnabled();
+        void Enable();
+        void Disable();
+
+        void AddComponent();
+        void GetComponent();
+        void HasComponent();
+
+    }
+}
